14387f8601948012d034ea3384fcd51977c214d7
aplumafreak500 <aplumafreak500@gmail.com>
2018-04-30 16:53:17 -0400

    Make version constant

--- source/version.c
+++
@@ -1,8 +0,0 @@
-#include <ogc/system.h>
-#include "version.h"
-
-int version=0;
-int subversion=0;
-int revision=0;
-int build=1;
-bool debug_build=false;
--- source/version.h
+++ source/version.h
@@ -1,5 +1,5 @@
-int version;
-int subversion;
-int revision;
-int build;
-bool debug_build;
+static u16 version=0;
+static u16 subversion=0;
+static u16 revision=0;
+static u16 build=2;
+static bool debug_build=false;
