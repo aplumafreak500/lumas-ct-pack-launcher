26f4c7aaae9d0ba9aaccdf6a6dc874db520f9d0e
aplumafreak500 <aplumafreak500@gmail.com>
2018-05-01 14:00:22 -0400

    These should be there

---
+++ source/ctgp/region.c
@@ -0,0 +1,5 @@
+#include <ogc/system.h>
+#include "region.h"
+
+static const s16 region=605;
+static const s16 cd_region=604;
---
+++ source/version.c
@@ -0,0 +1,8 @@
+#include <ogc/system.h>
+#include "version.h"
+
+static const u16 version=0;
+static const u16 subversion=0;
+static const u16 revision=0;
+static const u16 build=4;
+static const bool debug_build=true;
